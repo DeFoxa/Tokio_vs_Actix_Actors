472f0d58fe710072b963cbc6ccd0e3dc77b28765
Malaparte4 <113225642+Malaparte4@users.noreply.github.com>
2024-02-01 18:23:58 -0600

    kompact setup

---
+++ src/bin/testing_tui.rs
@@ -0,0 +1,105 @@
+#![allow(unused)]
+use actix::Actor;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use futures_util::StreamExt;
+use lib::concurrency_setup::actix_actor_model::*;
+use lib::concurrency_setup::tokio_actor_model::TradeStreamActorHandler;
+use lib::concurrency_setup::tokio_actor_model::{
+    MatchingEngineActor as MEA, MatchingEngineHandler, MatchingEngineMessage,
+    OrderBookActorHandler, OrderBookStreamMessage as OBSM, SequencerActor as SA, SequencerHandler,
+    SequencerMessage as SM, StateManagementMessage, TradeStreamActor as TSA,
+    TradeStreamMessage as TSM,
+};
+use lib::schema::binancetrades::dsl::*;
+use serde_json::Value;
+use tungstenite::Message;
+// use lib::{schema::binancetrades, types::*};
+use anyhow::Result;
+use lib::{
+    client::{ws::*, ws_types::*},
+    types::*,
+    utils::*,
+};
+
+use serde::Deserialize;
+use std::env;
+use std::io::stdout;
+
+use crossterm::{
+    event::{self, Event::Key, KeyCode::Char, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Frame, Stylize, Terminal},
+    widgets::Paragraph,
+};
+
+pub const MAINNET: &str = "wss://fstream.binance.com";
+
+fn main() -> Result<()> {
+    start_up()?;
+    let status = run();
+    shutdown()?;
+    status?;
+    Ok(())
+}
+
+struct App {
+    counter: i64,
+    should_quit: bool,
+}
+
+fn ui(app: &App, frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new(format!("Counter: {}", app.counter)),
+        frame.size(),
+    );
+}
+
+fn update(app: &mut App) -> Result<()> {
+    if event::poll(std::time::Duration::from_millis(250))? {
+        if let Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                match key.code {
+                    Char('j') => app.counter += 1,
+                    Char('k') => app.counter -= 1,
+                    Char('q') => app.should_quit = true,
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+    let mut app = App {
+        counter: 0,
+        should_quit: false,
+    };
+
+    loop {
+        t.draw(|f| ui(&app, f))?;
+        update(&mut app);
+        if app.should_quit {
+            break;
+        }
+    }
+    Ok(())
+}
+fn start_up() -> Result<()> {
+    enable_raw_mode()?;
+    execute!(std::io::stderr(), EnterAlternateScreen)?;
+    Ok(())
+}
+fn shutdown() -> Result<()> {
+    execute!(std::io::stderr(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
