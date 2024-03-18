6ae382fca3f7fc6ec8042270a8417181ec397d4d
Malaparte4 <113225642+Malaparte4@users.noreply.github.com>
2024-03-18 10:18:25 -0500

    removal: kompact and ob_model from lib.rs and concurrency_setup/mod.rs

--- src/concurrency_setup/mod.rs
+++ src/concurrency_setup/mod.rs
@@ -1,3 +1,2 @@
 pub mod actix_actor_model;
-pub mod kompact_hybrid_model;
 pub mod tokio_actor_model;
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 pub mod client;
 pub mod concurrency_setup;
 pub mod models;
-pub mod ob_model;
+// pub mod ob_model;
 pub mod schema;
 pub mod types;
 pub mod utils;
