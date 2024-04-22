64826e9d6991ca3185c04d860e08201147d2a7f9
Malaparte4 <113225642+Malaparte4@users.noreply.github.com>
2024-04-21 23:16:32 -0500

    bin/tokio_framework import/comment cleanup

--- src/bin/tokio_framework.rs
+++ src/bin/tokio_framework.rs
@@ -1,34 +1,19 @@
 #![allow(warnings)]
-use diesel::{prelude::*, PgConnection};
-use dotenvy::dotenv;
-use lib::concurrency_setup::actix_actor_model::*;
+use anyhow::Result;
+use futures_util::StreamExt;
 use lib::concurrency_setup::tokio_actor_model::TradeStreamActorHandler;
 use lib::concurrency_setup::tokio_actor_model::{
-    MatchingEngineActor as MEA, MatchingEngineHandler, MatchingEngineMessage,
-    OrderBookActorHandler, OrderBookStreamMessage as OBSM, SequencerActor as SA, SequencerHandler,
-    SequencerMessage as SM, StateManagementMessage, TradeStreamActor as TSA,
+    MatchingEngineHandler, OrderBookActorHandler, OrderBookStreamMessage as OBSM, SequencerHandler,
     TradeStreamMessage as TSM,
 };
-use lib::schema::binancetrades::dsl::*;
-use std::{collections::BinaryHeap, env};
-use tracing_subscriber::Layer;
-use tracing_subscriber::{filter::EnvFilter, fmt, prelude::*, registry::Registry};
-
-use actix::prelude::*;
-use actix_rt::{task::spawn_blocking, Arbiter, System};
-use anyhow::Result;
-use futures_util::{Stream, StreamExt};
-use lib::models::BinanceTradesNewModel;
 use lib::{
     client::{ws::*, ws_types::*},
     types::*,
-    utils::*,
 };
 use serde_json::Value;
-use std::time::{Duration, Instant};
-use tokio::sync::{mpsc, oneshot};
 use tokio_tungstenite::tungstenite::Message;
-use tracing_flame::FlameLayer;
+// use tracing_flame::FlameLayer;
+
 pub const MAINNET: &str = "wss://fstream.binance.com";
 
 #[tokio::main]
